08bb6c00c6253df39ec97960e6f39228d5020c21
정찬영 <jcy0342@gmail.com>
2023-11-14 23:25:25 +0900

    test: 주말 이벤트 적용 여부 테스트

--- src/test/java/christmas/domain/CalendarTest.java
+++ src/test/java/christmas/domain/CalendarTest.java
@@ -1,7 +1,6 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.constant.Event;
 import java.util.List;
@@ -33,6 +32,30 @@ class CalendarTest {
         List<Event> events = calendar.getEventsByDate(date);
         //When
         boolean isContain = events.contains(Event.크리스마스_디데이);
+        //Then
+        assertThat(isContain).isFalse();
+    }
+
+    @DisplayName("금요일과 토요일에는 주말 이벤트가 적용된다.")
+    @ValueSource(ints = {1, 30})
+    @ParameterizedTest
+    void validWeekendEvent(int date) {
+        //Given
+        List<Event> events = calendar.getEventsByDate(date);
+        //When
+        boolean isContain = events.contains(Event.주말_할인);
+        //Then
+        assertThat(isContain).isTrue();
+    }
+
+    @DisplayName("금요일과 토요일 외에는 주말 이벤트가 적용되지 않는다.")
+    @ValueSource(ints = {3, 28})
+    @ParameterizedTest
+    void invalidWeekendEvent(int date) {
+        //Given
+        List<Event> events = calendar.getEventsByDate(date);
+        //When
+        boolean isContain = events.contains(Event.주말_할인);
         //Then
         assertThat(isContain).isFalse();
     }
