3eb7f242529f05e3ba481db74d6a275a2e7b2a85
정찬영 <jcy0342@gmail.com>
2023-11-14 20:19:31 +0900

    feat: 방문 날짜를 올바르게 입력할 때까지 반복
    - 나중에 주문 입력에 대해서도 반복할 것이기 때문에 메서드로 분리

--- src/main/java/christmas/controller/Cashier.java
+++ src/main/java/christmas/controller/Cashier.java
@@ -1,11 +1,24 @@
 package christmas.controller;
 
 import christmas.view.InputView;
+import java.util.function.Supplier;
 
 public class Cashier {
     private final InputView inputView = new InputView();
 
     public void runAsManual() {
-        int date = inputView.readDate();
+        int date = doWorkUntilComplete(() -> {
+            return inputView.readDate();
+        });
+    }
+
+    private <T> T doWorkUntilComplete(Supplier<T> work) {
+        while (true) {
+            try {
+                return work.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
