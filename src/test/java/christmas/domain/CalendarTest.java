4c5ab429752d8aecaf23e80171450e15a6e5de0f
정찬영 <jcy0342@gmail.com>
2023-11-14 23:27:18 +0900

    test: 평일 이벤트 적용 여부 테스트

--- src/test/java/christmas/domain/CalendarTest.java
+++ src/test/java/christmas/domain/CalendarTest.java
@@ -37,7 +37,7 @@ class CalendarTest {
     }
 
     @DisplayName("금요일과 토요일에는 주말 이벤트가 적용된다.")
-    @ValueSource(ints = {1, 30})
+    @ValueSource(ints = {1, 2})
     @ParameterizedTest
     void validWeekendEvent(int date) {
         //Given
@@ -49,13 +49,37 @@ class CalendarTest {
     }
 
     @DisplayName("금요일과 토요일 외에는 주말 이벤트가 적용되지 않는다.")
-    @ValueSource(ints = {3, 28})
+    @ValueSource(ints = {3, 4, 5, 6, 7})
     @ParameterizedTest
     void invalidWeekendEvent(int date) {
         //Given
         List<Event> events = calendar.getEventsByDate(date);
         //When
         boolean isContain = events.contains(Event.주말_할인);
+        //Then
+        assertThat(isContain).isFalse();
+    }
+
+    @DisplayName("금요일과 토요일 외에는 평일 이벤트가 적용된다.")
+    @ValueSource(ints = {3, 4, 5, 6, 7})
+    @ParameterizedTest
+    void validWeekdayEvent(int date) {
+        //Given
+        List<Event> events = calendar.getEventsByDate(date);
+        //When
+        boolean isContain = events.contains(Event.평일_할인);
+        //Then
+        assertThat(isContain).isTrue();
+    }
+
+    @DisplayName("금요일과 토요일에는 평일 이벤트가 적용되지 않는다.")
+    @ValueSource(ints = {1, 2})
+    @ParameterizedTest
+    void invalidWeekdayEvent(int date) {
+        //Given
+        List<Event> events = calendar.getEventsByDate(date);
+        //When
+        boolean isContain = events.contains(Event.평일_할인);
         //Then
         assertThat(isContain).isFalse();
     }
